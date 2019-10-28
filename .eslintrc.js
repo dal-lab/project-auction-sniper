faacce119b2e21db2d644421b016b7d10fa84fdb
Ashal aka JOKER <ahastudio@gmail.com>
2019-10-29 02:09:00 +0900

    Use ESLint
    Use ESLint: https://eslint.org/
    
    Lint and fix all.

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules:  {
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+  },
+};
--- src/utils/chatService.ts
+++ src/utils/chatService.ts
@@ -2,28 +2,28 @@ import SendBird from 'sendbird';
 
 import pify from 'pify';
 
-const PF = (context: Object, f: Function) =>
+const PF = (context: Record<string, any>, f: Function): Function =>
   pify(f.bind(context), { errorFirst: false });
 
 export class ChatService {
-  async connect(appId: string, userId: string) {
+  async connect(appId: string, userId: string): Promise<void> {
     const sb = new SendBird({ appId });
     await PF(sb, sb.connect)(userId);
   }
 
-  async disconnect() {
+  async disconnect(): Promise<void> {
     const sb = SendBird.getInstance();
     await PF(sb, sb.disconnect)();
   }
 
-  async enter(channelUrl: string) {
+  async enter(channelUrl: string): Promise<void> {
     const sb = SendBird.getInstance();
     const oc = sb.OpenChannel;
     const channel = await PF(oc, oc.getChannel)(channelUrl);
     await PF(channel, channel.enter)();
   }
 
-  addListener(handlerId: string, events: Record<string, Function>) {
+  addListener(handlerId: string, events: Record<string, Function>): void {
     const sb = SendBird.getInstance();
     const handler = new sb.ChannelHandler();
     Object.entries(events).forEach(([name, f]) => {
